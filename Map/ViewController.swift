7c72b16984e095f13d16c9ef50f953a08c404784
maymayyim <mayyim.putti@gmail.com>
2017-11-09 11:38:50 +0700

    first

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func addPinToMap() {
+        let location_redBuild = CLLocationCoordinate2DMake(13.795746,100.325885)
+        let location_opBuild = CLLocationCoordinate2DMake(13.794388,100.325484)
+        let annotation_red = MapMarker(coordinate: location_redBuild, title: "EGCO Department", subtitle: "Phutamonthon, Salaya")
+        let annotation_op = MapMarker(coordinate: location_opBuild, title: "OP Building", subtitle: "Mahidol  University")
+        mapView.addAnnotation(annotation_red)
+        mapView.addAnnotation(annotation_op)
+        setCenterOfMapToLocation(location: location_redBuild)
+    }
+    
+    func setCenterOfMapToLocation(location: CLLocationCoordinate2D) {
+        let coordinate = CLLocationCoordinate2DMake(location.latitude, location.longitude)
+        let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        mapView.setRegion(region, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        addPinToMap()
     }
 
     override func didReceiveMemoryWarning() {
